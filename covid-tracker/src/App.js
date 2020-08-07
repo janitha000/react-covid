4a6f1e3b806f880fb5672d4af009f4f0ceb5e959
Janitha Tennakoon <janitha000@gmail.com>
2020-08-07 10:55:35 +0800

    Added InfoBox component

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -2,8 +2,11 @@ import React, { useState, useEffect } from 'react';
 import { FormControl, MenuItem, Select } from '@material-ui/core'
 import './App.css';
 
+import InfoBox from './components/InfoBox'
+
 const App = () => {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide')
 
   useEffect(() => {
     const getCountries = async () => {
@@ -20,20 +23,30 @@ const App = () => {
     getCountries()
   }, [])
 
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode)
+  }
+
+
   return (
     <div className="app">
       <div className="app__header">
         <h1> COVID-19 Tracker</h1>
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="worldwide">
+          <Select variant="outlined" value={country} onClick={onCountryChange}>
             <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
-
-
           </Select>
         </FormControl>
+      </div>
+
+      <div className="app__stats">
+        <InfoBox title="Corona Virus Cases" cases="1000" total="20000" />
+        <InfoBox title="Recovered" cases="1000" total="20000" />
+        <InfoBox title="Deaths" cases="1000" total="20000" />
       </div>
 
 
---
+++ covid-tracker/src/components/InfoBox.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core'
+import './InfoBox.css'
+
+const InfoBox = ({ title, cases, total }) => {
+    return (
+        <Card className="infoBox">
+            <CardContent>
+                <Typography color="textSecondary" className="infoBox__title">{title} </Typography>
+                <h2 className="infoBox__cases">{cases}</h2>
+                <Typography color="textSecondary" className="infoBox__total">{total} Total</Typography>
+            </CardContent>
+        </Card>
+    )
+
+}
+
+export default InfoBox;
