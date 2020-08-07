f168ffc6eb18b0edb7e64dee2528c5221d184857
Janitha Tennakoon <janitha000@gmail.com>
2020-08-07 10:10:50 +0800

    Added header

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { FormControl, MenuItem, Select } from '@material-ui/core'
 import './App.css';
 
 function App() {
   return (
-    <div >
-      <h1> Covid tracker</h1>
+    <div className="app">
+      <div className="app__header">
+        <h1> COVID-19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="Sri Lanka">Sri Lanka</MenuItem>
+            <MenuItem value="US">US</MenuItem>
+
+          </Select>
+        </FormControl>
+      </div>
+
+
     </div>
   );
 }
